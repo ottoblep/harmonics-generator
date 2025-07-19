fbb44f9adca0ee2b940792ed4c1260a815766472
Severin Lochschmidt <seviron53@gmail.com>
2025-07-19 11:44:40 +0200

    Add dissonance weight function

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,15 @@
 #include <cmath>
 
 int main() {
+
+    // Dissonance Weight Function
+    // exp(-0.84q) - exp(-1.38q) with q = (g-f) / 0.021f + 19
+    // g and f are two frequencies in Hz
+    auto dissonance_weight = [](double q) -> double {
+        // double q = (g - f) / ((0.021 * f) + 19.0);
+        return std::exp(-0.84 * q) - std::exp(-1.38 * q);
+    };
+
     using namespace plot;
     using namespace plot::ansi;
 
@@ -13,8 +22,8 @@ int main() {
     std::cout << clear;
 
     // Plot parameters
-    const int width = 80;
-    const int height = 20;
+    const int width = 150;
+    const int height = 50;
     const double pi = 3.14159265358979323846;
 
     // Draw axes
@@ -31,7 +40,7 @@ int main() {
     // Plot sine wave
     std::cout << foreground(plot::ansi::Color::Cyan);
     for (int x = 0; x < width; ++x) {
-        double value = std::sin(2 * pi * x / width);
+        double value = height * dissonance_weight(20 * x / width);
         int y = static_cast<int>((height / 2) - value * (height / 2 - 2));
         std::cout << move_to({x + 1, y + 1}) << "*";
     }
